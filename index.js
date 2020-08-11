9a89541d5e52fd23235c739a52ba00f2fdc4cfd4
alaaahmeddawabah <alaa.ahmed@ibtikar.net.sa>
2020-08-11 21:00:36 +0200

    add index file with a function to test

---
+++ index.js
@@ -0,0 +1,9 @@
+const mooncakeSays = message => {
+  let mooncake = "(o.o)";
+
+  if (!message) {
+    return `${mooncake} chookity?`;
+  }
+  return `${mooncake} ${message}`;
+};
+module.exports.mooncakeSays = mooncakeSays;
